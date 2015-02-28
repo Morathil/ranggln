a4a8f1c7ac6bab7755d15df67cc15de565ad6f1f
Christoph Altmann <christoph.altmann90@gmail.com>
2015-02-28 16:05:37 +0100

    Add gulp-connect and adjust gulpfile to reload after browserify (localhost:9000)

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var plugins = require("gulp-load-plugins")();
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
 var path = require('path');
+var connect = require("gulp-connect");
  
 var paths = {
 	scripts: ["src/js/**/*"],
@@ -32,10 +33,12 @@ gulp.task("browserify", function() {
 	});
 
 	bundleStream.pipe(source("index.js"))
-		.pipe(gulp.dest("www/js"));
+		.pipe(gulp.dest("www/js"))
+		.pipe(connect.reload());
 });
 
 gulp.task("default", ["vendor", "statics", "browserify"], function() {
+	connect.server({ root: "www", livereload: true, port: 9000 });
 	gulp.watch(paths.scripts, ["vendor", "statics", "browserify"]);
 	gulp.watch(paths.vendor, ["vendor", "statics", "browserify"]);
 	gulp.watch(paths.statics, ["vendor", "statics", "browserify"]);
--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,21 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function preload() {
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    //  You can fill the preloader with as many assets as your game requires
 
-        console.log('Received Event: ' + id);
-    }
-};
+    //  Here we are loading an image. The first parameter is the unique
+    //  string by which we'll identify the image later in our code.
 
-app.initialize();
+    //  The second parameter is the URL of the image (relative)
+    game.load.image('einstein', '../img/logo.png');
+
+}
+
+function create() {
+
+    //  This creates a simple sprite that is using our loaded image and
+    //  displays it on-screen
+    game.add.sprite(10, 0, 'einstein');
+
+}
