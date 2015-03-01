00cddcb257c86169180f05a721e5cabe14b88be3
Christoph Altmann <christoph.altmann90@gmail.com>
2015-03-01 01:08:58 +0100

    Add return to menu button in game

--- src/js/states/GameState.js
+++ src/js/states/GameState.js
@@ -1,9 +1,20 @@
 "use strict";
 
+var StateIds = require("./States");
+
 var GameState = function(game) {}
 
 var publicMethods = function() {
-  this.create = function() {};
+  this.create = function() {
+    this.game.add.button(400, 400, 'button', function() {
+        this.game.state.start(StateIds.MENU_STATE_ID);
+      },
+      this, 0, 1, 2);
+  };
+
+  this.resize = function() {
+    Game.scale.setGameSize(window.innerWidth, window.innerHeight);
+  };
 };
 
 var privateMethods = function() {};
